215822d5060ce9165ed5edfb0bcd7daec446963f
waterbottle0131 <cxm20080131@qq.com>
2026-09-26 19:36:47 +0800

    Add files via upload

---
+++ #模块导入.py
@@ -0,0 +1,24 @@
+#导入模块
+#以random。randint为例：
+
+#import 模块名
+# import random
+# for i in range(5):
+#     print(random.randint(1,100))
+
+#import 模块名 as 别名
+# import random as suijishu
+# for i in range(5):
+#     print(suijishu.randint(1,100))
+
+#from 模块名 import 功能名
+# from random import randint
+# print(randint(1,100))
+
+# from 模块名 import 功能名 as 别名
+# from random import randint as suijishu
+# print(suijishu(1,100))
+
+# from 模块名 import *
+from random import *
+print(randint(1,100))
